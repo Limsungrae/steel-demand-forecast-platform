a3fb309c8fdb7f44cd9e793ec2cc40626c0f962b
Limsungrae <tjdfo9708@gmail.com>
2026-06-04 14:45:41 +0900

    Initial Spring Boot Project

---
+++ src/main/java/com/steel/steeldemandplatform/SteelDemandPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.steel.steeldemandplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteelDemandPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteelDemandPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steel/steeldemandplatform/SteelDemandPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steel.steeldemandplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteelDemandPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
